dcf6e4285938d8ec69d98cdc0bf7a3c094c8059e
Benson <bensonxiao0721@gmail.com>
2026-03-16 21:11:58 -0400

    v2.0, fixed CORS error

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.dc import router as dc_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://widc.20age1million.com",
+        "http://localhost:3000",
+    ],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(dc_router)
